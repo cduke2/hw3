91a4982d2f71a015b7f618d52a0f5a9a046a7f73
cduke2 <caroline.duke@kellogg.northwestern.edu>
2022-02-22 23:06:25 +0000

    again

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
     end
   
     def create
-      @post = Post.new(params["post"])
+      @post = Post.new(params["title"])
       @post.save
       redirect_to "/posts"
     end
