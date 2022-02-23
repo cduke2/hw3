0c5473f14f2b82ad444bc39f2c89da73e3c0b110
cduke2 <caroline.duke@kellogg.northwestern.edu>
2022-02-23 00:36:26 +0000

    thank you anthony

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,13 @@ class PostsController < ApplicationController
 
     def new
       @post = Post.new
+      @post.place_id = params["place_id"]
     end
   
     def create
-      @post = Post.new(params["title"])
+      @post = Post.new(params["post"])
       @post.save
-      redirect_to "/posts"
+      redirect_to "/places"
     end
   
   end
